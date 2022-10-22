045ea0e47a3832d4a775e8c832c370f861bc4330
iBakuman <903926441@qq.com>
2022-10-22 21:22:20 +0800

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const { mainModule } = require('process')
+
+module.exports = {
+  // 使用 src/main.js 会报错
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+
+    ]
+  },
+  plugins: [
+
+  ],
+  mode: "development"
+}
